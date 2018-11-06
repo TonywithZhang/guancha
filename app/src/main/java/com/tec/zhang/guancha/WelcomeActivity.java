8df72d5efd4723d63f2525bcf236f06a638a7a15
Tony <757957839@qq.com>
2018-11-06 20:10:08 +0800

    daily release

--- app/src/main/java/com/tec/zhang/guancha/WelcomeActivity.java
+++ app/src/main/java/com/tec/zhang/guancha/WelcomeActivity.java
@@ -1,18 +1,31 @@
 package com.tec.zhang.guancha;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class WelcomeActivity extends AppCompatActivity {
+    private static final String TAG = "观察者网app";
+
+    //private LottieAnimationView w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12;
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -82,30 +95,113 @@ public class WelcomeActivity extends AppCompatActivity {
             return false;
         }
     };
-
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
+        ActionBar action = getSupportActionBar();
+        if (action != null){
+            action.hide();
+            Log.d(TAG, "onCreate: 程序已启动");
+        }
+        /*w1 = (LottieAnimationView) findViewById(R.id.w1);
+        w2 = (LottieAnimationView) findViewById(R.id.w2);
+        w3 = (LottieAnimationView) findViewById(R.id.w3);
+        w4 = (LottieAnimationView) findViewById(R.id.w4);
+        w5 = (LottieAnimationView) findViewById(R.id.w5);
+        w6 = (LottieAnimationView) findViewById(R.id.w6);
+        w7 = (LottieAnimationView) findViewById(R.id.w7);
+        w8 = (LottieAnimationView) findViewById(R.id.w8);
+        w9 = (LottieAnimationView) findViewById(R.id.w9);
+        w10 = (LottieAnimationView) findViewById(R.id.w10);
+        w11 = (LottieAnimationView) findViewById(R.id.w11);
+        w12 = (LottieAnimationView) findViewById(R.id.w12);*/
 
+        /*final List<LottieAnimationView> animalList = new ArrayList<>();
+        animalList.add(w1);
+        animalList.add(w2);
+        animalList.add(w3);*/
+
+        new CountDownTimer(3500,370){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                count ++;
+               /* for (LottieAnimationView ani : animalList){
+                    switch (count){
+                        case 1:
+                            w1.playAnimation();
+                            break;
+
+                        case 2:
+                            w2.playAnimation();
+                            break;
+
+                            case 3:
+                             w3.playAnimation();
+                             break;
+
+                        case 4:
+                            w4.playAnimation();
+                            break;
+                        case 5:
+                            w5.playAnimation();
+                            break;
+
+                        case 6:
+                            w6.playAnimation();
+                            break;
+                        case 7:
+                            w7.playAnimation();
+                            break;
+
+                        case 8:
+                            w8.playAnimation();
+                            break;
+
+                        case 9:
+                            w9.playAnimation();
+                            break;
+
+                        case 10:
+                            w10.playAnimation();
+                            break;
+                        case 11:
+                            w11.playAnimation();
+                            break;
+                        case 12:
+                            w12.playAnimation();
+                            break;
+                             default:
+                    }
+                }*/
+            }
+
+            @Override
+            public void onFinish() {
+
+                //Toast.makeText(WelcomeActivity.this,"动画加载完毕！！！",Toast.LENGTH_LONG).show();
+            }
+        }.start();
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        //mControlsView = findViewById(R.id.fullscreen_content_controls);
+        //mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+/*        mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 toggle();
             }
-        });
+        });*/
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
@@ -115,7 +211,7 @@ public class WelcomeActivity extends AppCompatActivity {
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
-        delayedHide(100);
+        //delayedHide(100);
     }
 
     private void toggle() {
